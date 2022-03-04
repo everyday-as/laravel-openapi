d0164e9b91fc6518b60b1d89b5b02aad3b3f432b
TheodoorP <tj.paganoni@gmail.com>
2022-03-04 18:39:26 +0100

    fix: properly handle headers

--- src/Builders/Components/ResponsesBuilder.php
+++ src/Builders/Components/ResponsesBuilder.php
@@ -11,22 +11,26 @@ class ResponsesBuilder extends Builder
 {
     public function build(string $collection = Generator::COLLECTION_DEFAULT): array
     {
+        $globalHeaderFactoryClasses = Arr::get(config('openapi'), 'collections.' . $collection . '.global_headers', []);
+
+        $globalHeaders = Arr::flatten(array_map(
+            static fn(string $factoryClass) => app($factoryClass)->build(),
+            $globalHeaderFactoryClasses
+        ));
+
         return $this->getAllClasses($collection)
             ->filter(static function ($class) {
                 return
                     is_a($class, ResponseFactory::class, true) &&
                     is_a($class, Reusable::class, true);
             })
-            ->map(function ($class) use ($collection) {
+            ->map(function ($class) use ($globalHeaders) {
                 /** @var ResponseFactory $instance */
                 $instance = app($class);
 
-                $globalHeaders = Arr::get(config('openapi'), 'collections.' . $collection . '.global_headers', []);
+                $response = $instance->build();
 
-                return optional(
-                        $globalHeaders,
-                        static fn(array $h) => $instance->build()->headers(...$h)
-                    ) ?? $instance->build();
+                return $response->headers(...$globalHeaders, ...$response->headers ?? []);
             })
             ->values()
             ->toArray();
