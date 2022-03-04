f332ae7fc56b02768a7b85691800e515baf96aab
TheodoorP <tj.paganoni@gmail.com>
2022-03-04 20:00:47 +0100

    fix: add header referencable

--- src/Concerns/Referencable.php
+++ src/Concerns/Referencable.php
@@ -2,10 +2,12 @@
 
 namespace Vyuldashev\LaravelOpenApi\Concerns;
 
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Header;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
 use InvalidArgumentException;
 use Vyuldashev\LaravelOpenApi\Contracts\Reusable;
 use Vyuldashev\LaravelOpenApi\Factories\CallbackFactory;
+use Vyuldashev\LaravelOpenApi\Factories\HeaderFactory;
 use Vyuldashev\LaravelOpenApi\Factories\ParametersFactory;
 use Vyuldashev\LaravelOpenApi\Factories\RequestBodyFactory;
 use Vyuldashev\LaravelOpenApi\Factories\ResponseFactory;
@@ -14,18 +16,22 @@ use Vyuldashev\LaravelOpenApi\Factories\SecuritySchemeFactory;
 
 trait Referencable
 {
-    public static function ref(?string $objectId = null): Schema
+    public static function ref(?string $objectId = null): Schema|Header
     {
         $instance = app(static::class);
 
-        if (! $instance instanceof Reusable) {
-            throw new InvalidArgumentException('"'.static::class.'" must implement "'.Reusable::class.'" in order to be referencable.');
+        if (!$instance instanceof Reusable) {
+            throw new InvalidArgumentException('"' . static::class . '" must implement "' . Reusable::class . '" in order to be referencable.');
         }
+
+        $foreignObjectId = $instance->build()->objectId;
 
         $baseRef = null;
 
         if ($instance instanceof CallbackFactory) {
             $baseRef = '#/components/callbacks/';
+        } else if ($instance instanceof HeaderFactory) {
+            return Header::ref('#/components/headers/' . $foreignObjectId, $objectId);
         } elseif ($instance instanceof ParametersFactory) {
             $baseRef = '#/components/parameters/';
         } elseif ($instance instanceof RequestBodyFactory) {
@@ -38,6 +44,6 @@ trait Referencable
             $baseRef = '#/components/securitySchemes/';
         }
 
-        return Schema::ref($baseRef.$instance->build()->objectId, $objectId);
+        return Schema::ref($baseRef . $foreignObjectId, $objectId);
     }
 }
